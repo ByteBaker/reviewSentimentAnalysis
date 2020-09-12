64bea0dd76145ee0b1c430edc0c9ff3b41a4474d
ByteBaker <shraddha.kishan@gmail.com>
2020-09-12 11:21:14 +0530

    Few cleanups

--- __main__.py
+++ __main__.py
@@ -1,7 +1,13 @@
-from reviewSetimentAnalysis import Scraper, Analysis 
+try:
+	from reviewSetimentAnalysis import Scraper, Analysis
+except ImportError:
+	from __init__ import Scraper, Analysis
 
-if __name__ == '__main__':
+def main():
 	url = input("Enter a Flipkart or Amazon product URL to analyze: \n")	
 	reviews = Scraper.get_reviews(url)	# Scraping reviews for this url
-	scores = Analysis.analyze(reviews, wordcloud=True, file_path='another\\temp.png')	# Analyzing scraped reviews
-	print(f'\nNegative {scores["neg"]:.2f}% Neutral {scores["neu"]:.2f}% Positive {scores["pos"]:.2f}%')
+	scores = Analysis.analyze(reviews, wordcloud=False, file_path='another\\temp.png')	# Analyzing scraped reviews
+	print(f'\nNegative {scores["neg"]:.2f}%, Neutral {scores["neu"]:.2f}%, Positive {scores["pos"]:.2f}%')
+
+if __name__ == '__main__':
+	main()
